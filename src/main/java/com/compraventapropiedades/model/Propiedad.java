f0218936435a46aa791fd1d7c6c8f141b156f04f
JuanVimon3 <jvillamonsalve@gmail.com>
2026-02-06 12:27:16 -0500

    define Propiedad entity

--- src/main/java/com/compraventapropiedades/model/Propiedad.java
+++ src/main/java/com/compraventapropiedades/model/Propiedad.java
@@ -1,5 +1,52 @@
 package com.compraventapropiedades.model;
 
 public class Propiedad {
+
+    private int idPropiedad;
+    private String ubicacion;
+    private int precio;
+    private int idUsuarioDueno;
+
+    public Propiedad() {}
+
+    public Propiedad(int idPropiedad, String ubicacion, int precio, int idUsuarioDueno) {
+        this.idPropiedad = idPropiedad;
+        this.ubicacion = ubicacion;
+        this.precio = precio;
+        this.idUsuarioDueno = idUsuarioDueno;
+    }
+
+    // Getters y setters 
     
+    public int getIdPropiedad() {
+        return idPropiedad;
+    }
+
+    public void setIdPropiedad(int idPropiedad) {
+        this.idPropiedad = idPropiedad;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getIdUsuarioDueno() {
+        return idUsuarioDueno;
+    }
+
+    public void setIdUsuarioDueno(int idUsuarioDueno) {
+        this.idUsuarioDueno = idUsuarioDueno;
+    }
 }
