cacc8b4c9cdf4359cb0582a492a2d0877cba819c
JuanVimon3 <jvillamonsalve@gmail.com>
2026-02-10 09:20:54 -0500

    WIP

--- src/main/java/com/compraventapropiedades/App.java
+++ src/main/java/com/compraventapropiedades/App.java
@@ -1,5 +1,12 @@
 package com.compraventapropiedades;
 
+import com.compraventapropiedades.dao.PropiedadDAO;
+import com.compraventapropiedades.dao.UsuarioDAO;
+import com.compraventapropiedades.model.Propiedad;
+import com.compraventapropiedades.model.Usuario;
+import java.util.List;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +15,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        UsuarioDAO uDAO = new UsuarioDAO();
+        PropiedadDAO pDAO = new PropiedadDAO();
+
+        Usuario nuevoUsuario = new Usuario(0, 15455678, "pepito123@gmail.com", "Pepito Pérez" );
+       
+        // Insertar un nuevo usuario
+        uDAO.insertarUsuario(nuevoUsuario);
+
+        // Leer o listar usuarios
+        uDAO.listarUsuarios();
+
+        // Crear una lista para ver usuarios
+
+        List<Usuario> lista = uDAO.listarUsuarios();
+
+        int idPepito = lista.get(0).getIdUsuario();
+
+        // Insertar nueva propiedad
+
+        Propiedad nuevaPropiedad = new Propiedad(0, "Calle 32 123 San Javier", 7800000, idPepito);
+        pDAO.insertarPropiedad(nuevaPropiedad);
+
+        // Eliminar propiedad
+
+        pDAO.eliminarPropiedad(idPepito);
+
+        //Eliminar usuario
+
+        uDAO.eliminarUsuario(idPepito); // En caso de que queramos borrar el usuario de Pepito Pérez
+
+
     }
 }
--- src/main/java/com/compraventapropiedades/database/Conexion.java
+++ src/main/java/com/compraventapropiedades/database/Conexion.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost3306/compra_venta_propiedades";
+    private static final String URL = "jdbc:mysql://localhost:3306/compra_venta_propiedades";
     private static final String USER = "root";
     private static final String PASS = "Dracov3@2025";
 
@@ -16,7 +16,7 @@ public class Conexion {
             con = DriverManager.getConnection(URL, USER, PASS);
             System.out.println("Conexión exitosa a la base de datos de compra y venta de propiedades");
         } catch (Exception e) {
-            System.out.println("Error: conexión fallida" + e.getMessage());;
+            System.out.println("Error: conexión fallida " + e.getMessage());;
         }
 
         return con;
